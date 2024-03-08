ee1d804963cbe6bd9993ef571ccfdd78801465ac
hetvi333 <hetvisoni4922@gmail.com>
2024-03-08 17:26:36 +0530

    changes done

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,18 +43,7 @@ const Header = () => {
           />
         </Navbar.Text>
         <Nav>
-          <DropdownButton
-            title="Dropdown right"
-            menuAlign=""
-            id="dropdown-menu-align-right"
-          >
-            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-          </DropdownButton>
-          <Dropdown alignRight as={ButtonGroup}>
+           <Dropdown alignRight as={ButtonGroup}>
             <Dropdown.Toggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
               <Badge>{cart.length}</Badge>
@@ -95,7 +84,7 @@ const Header = () => {
                 <span style={{ padding: 10 }}>Cart is empty</span>
               )}
             </Dropdown.Menu>
-          </Dropdown>
+          </Dropdown> 
         </Nav>
       </Container>
     </Navbar>
